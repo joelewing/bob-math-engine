c8c80107e068b32723b9f3c780196577b642eda1
joelewing <joelewing@users.noreply.github.com>
2023-06-04 19:48:46 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,5 @@
+from math_utils import process_math_input
+
+user_input ="What is 5.5 plus 3.2 plus 2.3 and what is the river Thames? And also, what is 4.1 times 2.2 multiplied by 2.3? And what is the square root of 16.4? And what is 10.6 minus 7.7 minus 2.2? And what is 15.5 divided by 3.3 divided by 5.5? Additionally, what is 2 plus 4 times 3 factorial?"
+
+print(process_math_input(user_input))
